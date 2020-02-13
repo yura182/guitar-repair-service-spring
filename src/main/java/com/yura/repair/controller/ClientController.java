feaf483d45c1005e6e5c7f004813a14443bb331c
Yura <yuriy.petrashenko7@gmail.com>
2020-02-13 12:44:17 +0200

    add client review controller

--- src/main/java/com/yura/repair/controller/ClientController.java
+++ src/main/java/com/yura/repair/controller/ClientController.java
@@ -2,10 +2,13 @@ package com.yura.repair.controller;
 
 import com.yura.repair.dto.InstrumentDto;
 import com.yura.repair.dto.OrderDto;
+import com.yura.repair.dto.ReviewDto;
 import com.yura.repair.dto.UserDto;
 import com.yura.repair.entity.Status;
 import com.yura.repair.service.OrderService;
+import com.yura.repair.service.ReviewService;
 import lombok.AllArgsConstructor;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,16 +20,20 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 @Controller
 public class ClientController {
     private final OrderService orderService;
+    private final ReviewService reviewService;
 
     @GetMapping("/client/orders")
     public ModelAndView userOrders(@PageableDefault(size = 5, sort = "id", direction = Sort.Direction.DESC) Pageable pageable,
@@ -86,6 +93,27 @@ public class ClientController {
 
         modelAndView.setViewName("redirect:/client/add-order");
         redirectAttributes.addFlashAttribute("successMessage", "order.success");
+
+        return modelAndView;
+    }
+
+    @PostMapping("client/leave-review")
+    public ModelAndView leaveReview(@RequestParam(name = "orderId") @NotNull Integer orderId,
+                                    @RequestParam(name = "text") @NotBlank String text,
+                                    @AuthenticationPrincipal UserDto client,
+                                    ModelAndView modelAndView, RedirectAttributes redirectAttributes) {
+
+        ReviewDto review = ReviewDto.builder()
+                .client(client)
+                .orderDto(OrderDto.builder().id(orderId).build())
+                .date(LocalDateTime.now())
+                .text(text)
+                .build();
+
+        reviewService.add(review);
+
+        modelAndView.setViewName("redirect:/client/order/" + orderId);
+        redirectAttributes.addFlashAttribute("successMessage", "user.order.details.review.success");
 
         return modelAndView;
     }
