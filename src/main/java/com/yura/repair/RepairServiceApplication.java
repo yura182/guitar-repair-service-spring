816e1b93472b575e505a8fcbc2a1d22f57b30d10
Yura <yuriy.petrashenko7@gmail.com>
2020-06-08 20:31:17 +0300

    Add docker-compose

--- src/main/java/com/yura/repair/RepairServiceApplication.java
+++ src/main/java/com/yura/repair/RepairServiceApplication.java
@@ -2,10 +2,17 @@ package com.yura.repair;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RepairServiceApplication {
+public class RepairServiceApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(RepairServiceApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(RepairServiceApplication.class);
     }
 }
